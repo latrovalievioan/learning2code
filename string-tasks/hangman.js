4464cc96a9cd7e5e6b7f8c6acb7688edd9a2d89d
Ioan Latrovaliev <latrovalievioan@gmail.com>
2020-10-09 16:18:49 +0300

    Add hangman task in js

---
+++ string-tasks/hangman.js
@@ -0,0 +1,16 @@
+function hangman(phrase, arr) {
+    let result = ''
+    const regex = RegExp(arr.join('|') + '| ', 'si');
+    const regexAlphabetical = /^[A-Z]+$/i
+    for(let i = 0; i < phrase.length; i++){
+        result += ((regex.test(phrase[i]) && arr.length > 0) 
+        || !regexAlphabetical.test(phrase[i]))
+        ? 
+        phrase[i] : '-';
+    }
+    return result;
+}
+
+
+
+console.log(hangman("Looney Tunes", ["a", "e", "i", "o", "u"]))
