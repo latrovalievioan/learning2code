ffcd327147bc0246fb75e9e0399578f7caa00ef6
Ioan Latrovaliev <latrovalievioan@gmail.com>
2020-10-10 18:15:39 +0300

    Add east-to-west task in js Task can be found at: https://edabit.com/challenge/zptHytXMgKcAjr4TH

---
+++ string-tasks/eastToWest.js
@@ -0,0 +1,12 @@
+function direction(arr) {
+    const result = arr.map(x => {
+        return x.replace('e','w')
+        .replace('E','W')
+        .replace('a','e')
+        .replace('A','E');
+    });
+    return result;
+}
+
+
+console.log(direction(["east", "EAST", "eastEAST"]))
