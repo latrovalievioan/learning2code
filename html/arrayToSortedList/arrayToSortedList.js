dcb1ae3c8259999a324bea3b6493aff8a43d814f
Ioan Latrovaliev <latrovalievioan@gmail.com>
2020-10-18 10:59:17 +0300

    Add array-to-sorted-list task in js and html

---
+++ html/arrayToSortedList/arrayToSortedList.js
@@ -0,0 +1,9 @@
+//Take an array of names and outputs them as a sorted html list
+
+const names = [
+    "Konsalsin", "Merto", "Maria", "Eli", "Ventci",
+     "Ventsi", "Georgy", "Kiro", "Pesho", "Gosho"
+];
+
+const list = `<ol>${names.map(name => '<li>' + name + '</li>')}</ol>`;
+console.log(list.replace(/,/g,''));
--- test.js
+++ test.js
@@ -1,1 +1,2 @@
-console.log(8 % 4)
+const obj = {'Animal': {'Name' : 'dog', 'Eyes' : 2, 'Head' : 1, 'Paws' : 4 , 'Cute-face' : 1}}
+console.log(obj.Animal['Cute-face'])
