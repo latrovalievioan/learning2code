21ab5b5b23cd91dce601932384a488f05866349c
Ioan Latrovaliev <latrovalievioan@gmail.com>
2020-11-25 13:22:18 +0200

    Fix borders implementation.

--- apps/moveableObject/object/object.js
+++ apps/moveableObject/object/object.js
@@ -4,38 +4,14 @@ const DragabbleObject = () => {
   let offsetX = 25;
   let offsetY = 25;
   const onMouseMove = (event) => {
-    const mouseX = event.clientX;
-    const mouseY = event.clientY;
-    ///bottom border
-    if (
-      mouseY - offsetY >
-      object.parentElement.offsetHeight -
-        (object.getBoundingClientRect().bottom -
-          object.getBoundingClientRect().top)
-    ) {
-      ///top border
-    } else if (
-      mouseY - offsetY <
-      object.parentElement.getBoundingClientRect().top
-    ) {
-    }
-    ///left border
-    else if (
-      mouseX - offsetX <
-      object.parentElement.getBoundingClientRect().left
-    ) {
-    }
-    ///right border
-    else if (
-      mouseX - offsetX >
-      object.parentElement.getBoundingClientRect().right -
-        (object.getBoundingClientRect().right -
-          object.getBoundingClientRect().left)
-    ) {
-    } else {
-      object.style = `top:${mouseY - offsetY}px;
-      left:${mouseX - offsetX}px`;
-    }
+    const oRect = object.getBoundingClientRect();
+    const pRect = object.parentElement.getBoundingClientRect();
+    const maxTop = pRect.bottom - oRect.height;
+    const maxLeft = pRect.right - oRect.width;
+    const newTop = Math.max(0, Math.min(event.clientY - offsetY, maxTop));
+    const newLeft = Math.max(0, Math.min(event.clientX - offsetX, maxLeft));
+    object.style.top = `${newTop}px`;
+    object.style.left = `${newLeft}px`;
   };
   object.addEventListener("mousedown", (event) => {
     document.addEventListener("mousemove", onMouseMove);
