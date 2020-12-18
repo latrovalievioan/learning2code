ec0abd0d3ef8e8aec897f9c9708ff9ecd240ea4a
Ioan Latrovaliev <latrovalievioan@gmail.com>
2020-12-18 21:38:06 +0200

    Implement reverse and add

--- apps/linkedList/main.js
+++ apps/linkedList/main.js
@@ -31,7 +31,6 @@ const Append = () => {
   const button = mkElem("button", { class: "append" });
   button.innerText = "Append";
   button.addEventListener("click", () => {
-    console.log(field.value);
     list.append(field.value);
     field.value = "";
     renderList("linked-list", list);
@@ -52,18 +51,49 @@ const Prepend = () => {
   return { button, field };
 };
 
+const Reverse = () => {
+  const button = mkElem("button", { class: "reverse" });
+  button.innerText = "Reverse";
+  button.addEventListener("click", () => {
+    list.reverse();
+    renderList("linked-list", list);
+  });
+  return { domElement: button };
+};
+
+const At = () => {
+  const field = mkElem("input", { type: "text", class: "at" });
+  const button = mkElem("button", { class: "at" });
+  button.innerText = "At";
+  button.addEventListener("click", () => {
+    for (let i = 0; i < document.getElementsByClassName("Node").length; i++) {
+      if (i === Number(field.value)) {
+        document.getElementsByClassName("Node")[i].classList.add("highlighted");
+      }
+    }
+  });
+  return { button, field };
+};
+
 const renderControls = (containerId) => {
   const container = document.getElementById(containerId);
   const append = Append();
   const prepend = Prepend();
+  const reverse = Reverse();
+  const at = At();
   const appendContainer = mkElem("div", { class: "append-container" });
   appendContainer.appendChild(append.button);
   appendContainer.appendChild(append.field);
   const prependContainer = mkElem("div", { class: "prepend-container" });
   prependContainer.appendChild(prepend.button);
   prependContainer.appendChild(prepend.field);
+  const atContainer = mkElem("div", { class: "at-container" });
   container.appendChild(appendContainer);
   container.appendChild(prependContainer);
+  container.appendChild(reverse.domElement);
+  atContainer.appendChild(at.button);
+  atContainer.appendChild(at.field);
+  container.appendChild(atContainer);
 };
 
 renderList("linked-list", list);
