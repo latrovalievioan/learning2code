1c1beaf82592f1ba7983583dfae63cf9b9f06451
Ioan Latrovaliev <latrovalievioan@gmail.com>
2021-01-11 17:38:45 +0200

    Do some async stuff"

--- async/async.js
+++ async/async.js
@@ -7,4 +7,23 @@
 
 // sleep(9000).then(() => console.log("zdrawei kote"));
 
-setInterval(() => console.log("koko e krasiv"), 1000);
+// setInterval(() => console.log("koko e krasiv"), 1000);
+
+const fs = require("fs");
+const { resolve } = require("path");
+// fs.readFile("./test.js", "utf-8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+const readFilePromise = (file) =>
+  new Promise((resolve, reject) => {
+    fs.readFile(file, "utf-8", (err, data) => {
+      if (err) reject(err);
+      else {
+        resolve(data);
+      }
+    });
+  });
+
+readFilePromise("./test.js").then((data) => console.log(data));
