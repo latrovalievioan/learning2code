c53fda3bb14b71fb29cc4dc1a08d6edb694e5c0f
Ioan Latrovaliev <latrovalievioan@gmail.com>
2021-01-22 14:10:59 +0200

    Animate bouncing ball

--- canvasCrashCourse/main.js
+++ canvasCrashCourse/main.js
@@ -52,30 +52,70 @@ const ctx = canvas.getContext("2d");
 
 //Draw Smiley Facey
 
-ctx.beginPath();
+// ctx.beginPath();
 
-const centerX = canvas.width / 2;
-const centerY = canvas.height / 2;
+// const centerX = canvas.width / 2;
+// const centerY = canvas.height / 2;
 
-//Draw Head
-ctx.arc(centerX, centerY, 200, 0, Math.PI * 2);
+// //Draw Head
+// ctx.arc(centerX, centerY, 200, 0, Math.PI * 2);
 
-//Move to mouth
-ctx.moveTo(centerX + 100, centerY);
+// //Move to mouth
+// ctx.moveTo(centerX + 100, centerY);
 
-// Draw Mouth
-ctx.arc(centerX, centerY, 100, 0, Math.PI, false);
+// // Draw Mouth
+// ctx.arc(centerX, centerY, 100, 0, Math.PI, false);
 
-//Move to left eye
-ctx.moveTo(centerX - 60, centerY - 80);
+// //Move to left eye
+// ctx.moveTo(centerX - 60, centerY - 80);
 
-//Draw left eye
-ctx.arc(centerX - 80, centerY - 80, 20, 0, Math.PI * 2);
+// //Draw left eye
+// ctx.arc(centerX - 80, centerY - 80, 20, 0, Math.PI * 2);
 
-//Move to right eye
-ctx.moveTo(centerX + 100, centerY - 80);
+// //Move to right eye
+// ctx.moveTo(centerX + 100, centerY - 80);
 
-//Draw right eye
-ctx.arc(centerX + 80, centerY - 80, 20, 0, Math.PI * 2);
+// //Draw right eye
+// ctx.arc(centerX + 80, centerY - 80, 20, 0, Math.PI * 2);
 
-ctx.stroke();
+// ctx.stroke();
+
+//Animation 1
+
+// const circle = {
+//   x: 200,
+//   y: 200,
+//   size: 30,
+//   dx: 5,
+//   dy: 4,
+// };
+
+// const drawCircle = (circle) => {
+//   ctx.beginPath();
+//   ctx.arc(circle.x, circle.y, circle.size, 0, Math.PI * 2);
+//   ctx.fillStyle = "cyan";
+//   ctx.fill();
+// };
+
+// const update = () => {
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
+//   drawCircle(circle);
+
+//   //change position
+//   circle.x += circle.dx;
+//   circle.y += circle.dy;
+
+//   //detect side walls
+//   if (circle.x + circle.size > canvas.width || circle.x - circle.size < 0) {
+//     circle.dx *= -1;
+//   }
+
+//   //detect top&bot walls
+//   if (circle.y + circle.size > canvas.height || circle.y - circle.size < 0) {
+//     circle.dy *= -1;
+//   }
+
+//   requestAnimationFrame(update);
+// };
+
+// update();
