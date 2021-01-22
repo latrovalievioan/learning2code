d4bf1e9c833ee4eb1bfef6712f2308cac2eafd7f
Ioan Latrovaliev <latrovalievioan@gmail.com>
2021-01-22 13:05:00 +0200

    Learn to draw Rectangles and text in canvas

---
+++ canvasCrashCourse/main.js
@@ -0,0 +1,22 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+//creating rectangles
+//fillRect(x, y, w, h)
+// ctx.fillStyle = "red";
+// ctx.fillRect(20, 20, 100, 100);
+// ctx.fillStyle = "blue";
+// ctx.fillRect(500, 20, 50, 50);
+
+// //strokeRect()
+// ctx.lineWidth = "5";
+// ctx.strokeStyle = "red";
+// ctx.strokeRect(20, 170, 100, 100);
+
+// //clearRect()
+// ctx.clearRect(25, 25, 30, 30);
+
+// //fillText('text', x, y)
+// ctx.font = "30px Cursive";
+// ctx.fillStyle = "yellow";
+// ctx.fillText("Ucha Canvas Brat", 250, 250);
