053273dbd6e3d9765ebcaa60e873d5efe4157e6b
Ioan Latrovaliev <latrovalievioan@gmail.com>
2021-06-23 13:51:23 +0300

    manage to get the table with all moves

--- algorithms/shortestPath.js
+++ algorithms/shortestPath.js
@@ -37,8 +37,8 @@ const paths = {
 };
 
 const size = 7;
-const startR = 0;
-const startC = 3;
+const start = { r: 0, c: 3, name: 'start' };
+const end = { r: 4, c: 3 };
 
 const initMatrix = (size) => {
   return Array.from({ length: size }, () =>
@@ -48,22 +48,76 @@ const initMatrix = (size) => {
 
 const emptyMatrix = initMatrix(size);
 
-const findPossiblePaths = (size, startR, startC, emptyMatrix) => {
-  emptyMatrix[startR][startC] = 'started';
+const bfs = () => {
+  const q = [];
+  const visited = {};
 
-  for (path in paths) {
-    const pathCoordinates = paths[path];
-    const pathName = path;
-    const pathR = pathCoordinates.r;
-    const pathC = pathCoordinates.c;
-    const newR = startR + pathR;
-    const newC = startC + pathC;
+  q.push(start);
 
-    if (newR < size && newC < size && newR >= 0 && newC >= 0) {
-      emptyMatrix[startR + pathR][startC + pathC] = pathName;
-    }
+  while (q.length > 0) {
+    const { r, c } = q.shift();
+    const currentKey = `${r}x${c}`;
+    const current = emptyMatrix[r][c];
+    const neighbors = [
+      { r: r + paths.ul.r, c: c + paths.ul.c, name: 'UL' },
+      { r: r + paths.ur.r, c: c + paths.ur.c, name: 'UR' },
+      { r: r + paths.r.r, c: c + paths.r.c, name: 'R' },
+      { r: r + paths.lr.r, c: c + paths.lr.c, name: 'LR' },
+      { r: r + paths.ll.r, c: c + paths.ll.c, name: 'LL' },
+      { r: r + paths.l.r, c: c + paths.l.c, name: 'L' },
+    ];
+
+    neighbors.forEach((n) => {
+      const nRow = n.r;
+      const nCol = n.c;
+
+      if (nRow === end.r && nCol === end.c) {
+        console.log('yay');
+      }
+      if (
+        nRow < 0 ||
+        nRow > emptyMatrix.length - 1 ||
+        nCol < 0 ||
+        nCol > emptyMatrix.length - 1
+      )
+        return;
+
+      const key = `${nRow}x${nCol}`;
+
+      if (key in visited) return;
+
+      visited[key] = {
+        key: currentKey,
+        cell: current,
+      };
+
+      emptyMatrix[nRow][nCol] = n.name;
+
+      q.push(n);
+    });
   }
-  return emptyMatrix;
 };
 
-console.table(findPossiblePaths(size, startR, startC, emptyMatrix));
+bfs();
+
+console.table(emptyMatrix);
+
+// const findPossiblePaths = (size, startR, startC, emptyMatrix) => {
+//   emptyMatrix[startR][startC] = 'started';
+
+//   for (path in paths) {
+//     const pathCoordinates = paths[path];
+//     const pathName = path;
+//     const pathR = pathCoordinates.r;
+//     const pathC = pathCoordinates.c;
+//     const newR = startR + pathR;
+//     const newC = startC + pathC;
+
+//     if (newR < size && newC < size && newR >= 0 && newC >= 0) {
+//       emptyMatrix[startR + pathR][startC + pathC] = pathName;
+//     }
+//   }
+//   return emptyMatrix;
+// };
+
+// console.table(findPossiblePaths(size, startR, startC, emptyMatrix));
