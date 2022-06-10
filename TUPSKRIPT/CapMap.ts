decc58cd71314f3a4cfe86aced6db77891dccd64
Ioan Latrovaliev <latrovalievioan@gmail.com>
2022-06-10 15:52:49 +0300

    second way to implement CapMap

--- TUPSKRIPT/CapMap.ts
+++ TUPSKRIPT/CapMap.ts
@@ -8,4 +8,8 @@ type CapMap<T> = {
   [Property in keyof T]: T[Property] extends string ? Capitalize<T[Property]> : T[Property]
 }
 
+type CapMap1<T extends Record<string, string>> = {
+  [Property in keyof T]: Capitalize<T[Property]>
+}
+
 type x = CapMap<A> // { a: "Gosho"; b: "Pesho" }
